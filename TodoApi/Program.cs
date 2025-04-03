d0f954c1dba80a3b8263465e8934b6d07a57ec73
Rachel Dempsey <rachelmyers52@gmail.com>
2025-04-03 11:10:11 -0500

    Add project files.

---
+++ TodoApi/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+
+// Register CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000") // Allow frontend
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+
+/*policy =>
+{
+    policy.WithOrigins("http://localhost:300") // Allow frontend
+          .AllowAnyMethod()
+          .AllowAnyHeader();
+})*/;
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Use CORS
+app.UseCors("AllowFrontend");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ TodoApiUnitTests/TodoItemsControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using TodoApi.Controllers;
+using TodoApi.Models;
+
+namespace TodoApiUnitTests
+{
+    [TestClass]
+    public sealed class TodoItemsControllerTests
+    {
+
+        [TestMethod]
+        public async void GetTodoItems_ValidID_ReturnItem()
+        {
+            //Arrange
+            var item = new TodoItem
+            {
+                Id = 1,
+                Name = "Complete unit testing setup",
+                IsComplete = true,
+                Secret = ""
+            };
+
+            //var context = new TodoContext();
+
+
+            //TodoItemsController controller = new TodoItemsController();
+
+            //Assert.IsNotNull(controller.GetTodoItem(1));
+            
+            
+        }
+
+        public void GetTodoItems_InvalidID_ReturnNotFound()
+        {
+
+        }
+
+        public void GetTodoItems_NullID_ReturnNotFound()
+        {
+
+        }
+    }
+}
